3e18afb42cdc29c26ac874b3f9848d1e2ca2741c
arshitas <71453910+arshitas@users.noreply.github.com>
2021-06-11 13:15:18 +0530

    Add files via upload

---
+++ surveillance_arduino.ino
@@ -0,0 +1,73 @@
+//Mini surveillance system
+
+int motorPin1 =  5;    // Motor connected at pwm pin 5&6
+int motorPin2 =  6;    
+int IRsensor=2;        //IR sensor output connected at pwm pin2
+
+String str="*";
+float ival;
+// The setup() method runs once, when the sketch starts
+
+void setup()   {                
+  // initialize the digital pins as an output:
+  pinMode(motorPin1, OUTPUT); 
+  pinMode(motorPin2, OUTPUT);  
+  pinMode(IRsensor,INPUT);
+  
+  Serial.begin(4800);
+  
+}
+
+
+//40 deg turn in 1 s
+//20 deg turn in 500ms
+//180 deg turn in 4500 ms
+// the loop() method runs over and over again,
+// as long as the Arduino has power
+void loop()                     
+{
+  rotateCW();
+  rotateCCW();
+  
+}
+
+
+void rotateCW(void){
+  digitalWrite(motorPin1, HIGH); //rotates motor
+  digitalWrite(motorPin2, LOW);    // set the Pin motorPin2 LOW
+  for(int i=0;i<=180;i=i+2)
+  {
+    int status=digitalRead(IRsensor);
+    if(status==HIGH)
+    {
+      ival=2.4;
+    }
+    else
+    {
+      ival=0.8;
+    }
+    delay(45);
+    Serial.println(i+str+ival);
+  }
+  digitalWrite(motorPin1, LOW); 
+}
+
+void rotateCCW(void){
+  digitalWrite(motorPin2, HIGH); //rotates motor
+  digitalWrite(motorPin1, LOW);    // set the Pin motorPin1 LOW
+  for(int i=0;i<=180;i=i+2)
+  {
+    int status=digitalRead(IRsensor);
+    if(status==HIGH)
+    {
+      ival=2.4;
+    }
+    else
+    {
+      ival=0.8;
+    }
+    delay(65);
+    Serial.println(i+str+ival);
+  }
+   digitalWrite(motorPin2, LOW);
+}
